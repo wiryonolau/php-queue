ad898b1781df7c992ea9ae86847a853a1320f48d
Wiryono <wiryonolau@gmail.com>
2021-07-29 05:54:12 +0700

    Add Module

---
+++ src/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Itseasy\Queue;
+
+class Module
+{
+    public static function getConfigPath() : array {
+        return [
+            __DIR__."/../config/*.{local,config}.php",
+        ]
+    }
+}
